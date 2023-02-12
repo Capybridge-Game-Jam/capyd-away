98e8847846c5e782f927d073e3f8efadc544757b
Ronan Ragavoodoo <porkypus@gmail.com>
2023-02-12 04:07:00 +0000

    shrooms

---
+++ Assets/BouncyShrooms.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BouncyShrooms : MonoBehaviour
+{
+    public Vector3 teleportPosition;
+    public GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // Write code to make the player bounce off the object on collision
+    
+}
